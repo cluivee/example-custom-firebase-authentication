cbe69c4bdc3350470ef6b3f610470b866f6884c5
Clive <cluivee@gmail.com>
2022-07-18 17:43:15 +0100

    Forget and delete button works

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
   GoogleAuthProvider,
   FacebookAuthProvider,
   signInWithPopup,
+  sendPasswordResetEmail,
+  deleteUser,
 } from "firebase/auth";
 // import SignIn from "./SignIn";
 // import SignUp from "./SignUp";
@@ -58,9 +60,6 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
-// the current logged in user
-const user = auth.currentUser;
-
 const provider = new GoogleAuthProvider();
 
 // const displayName = "";
@@ -83,7 +82,6 @@ const provider = new GoogleAuthProvider();
 
 //   // uid = user.uid;
 // }
-
 
 // 18.07.2022 this is slightly buggy, as it won't say "sign in successful" on successful login here, but I'm keeping this method outside
 // the App component because it causes problems with asking for the email 4 times if I put it in the app component.
@@ -139,26 +137,18 @@ function Copyright(props) {
 const theme = createTheme();
 
 function SignUp(props) {
-  const [signUpErrorText, setsignUpErrorText] = useState('')
+  const [signUpErrorText, setsignUpErrorText] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-      type: typeof data.get("email"),
-    });
 
     if (!isEmail(data.get("email"))) {
-      console.log("Email invalid, please enter a valid email");
       setsignUpErrorText("Not a valid email, please enter a valid email");
-      
     } else if (!isStrongPassword(data.get("password"), { minUppercase: 0 })) {
-      console.log(
+      setsignUpErrorText(
         "Password is not strong enough, min characters: 8, min numerical: 1, min symbols: 1,  please choose another password"
       );
-      setsignUpErrorText("Password is not strong enough, min characters: 8, min numerical: 1, min symbols: 1,  please choose another password")
     } else if (
       isEmail(data.get("email")) &&
       isStrongPassword(data.get("password"), { minUppercase: 0 })
@@ -172,11 +162,20 @@ function SignUp(props) {
           // Signed in
           const user = userCredential.user;
           props.setSignInSuccessfulText("Sign in successful!");
+          setsignUpErrorText("Account Successfully Created!");
           // ...
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
+          console.log(errorCode);
+
+          if (errorCode === "auth/email-already-in-use") {
+            setsignUpErrorText("Account with this email already exists");
+          } else {
+            setsignUpErrorText("Something went wrong with sign in");
+          }
+
           // ..
         });
 
@@ -199,8 +198,8 @@ function SignUp(props) {
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5" sx={{ fontSize: "48px" }}>
-            Create a New Account
+          <Typography component="h1" variant="h2" align="center">
+            Create A New Account
           </Typography>
           <Box
             component="form"
@@ -209,27 +208,6 @@ function SignUp(props) {
             sx={{ mt: 3 }}
           >
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
@@ -252,7 +230,13 @@ function SignUp(props) {
                 />
               </Grid>
               <Grid item xs={12}>
-                <div style={{fontSize:'0.75rem', color: 'red'}}>{signUpErrorText}</div>
+                <Typography
+                  component="h1"
+                  variant="caption"
+                  sx={{ color: "red" }}
+                >
+                  {signUpErrorText}
+                </Typography>
               </Grid>
             </Grid>
             <Button
@@ -281,6 +265,19 @@ function SignUp(props) {
 // SignIn MUI Template Component
 
 function SignIn(props) {
+  const [signInErrorText, setsignInErrorText] = useState("");
+
+  // sendPasswordResetEmail(auth, email)
+  // .then(() => {
+  //   // Password reset email sent!
+  //   // ..
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   // ..
+  // });
+
   const handleSignInSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -293,20 +290,16 @@ function SignIn(props) {
     );
 
     if (!isEmail(data.get("email"))) {
-      console.log("email invalid, please enter a valid email");
-    } else if (!isStrongPassword(data.get("password"), { minUppercase: 0 })) {
-      console.log(
-        "password is not strong enough, please choose another password"
-      );
-    } else if (
-      isEmail(data.get("email")) &&
-      isStrongPassword(data.get("password"), { minUppercase: 0 })
-    ) {
+      setsignInErrorText("Not a valid email, please enter a valid email");
+    } else if (data.get("password").length < 6) {
+      setsignInErrorText("Password invalid");
+    } else if (isEmail(data.get("email")) && data.get("password").length >= 6) {
       signInWithEmailAndPassword(auth, data.get("email"), data.get("password"))
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
           props.setSignInSuccessfulText("Sign in successful!");
+          setsignInErrorText("");
 
           // ...
         })
@@ -314,6 +307,12 @@ function SignIn(props) {
           console.log("no user with these credentials exists");
           const errorCode = error.code;
           const errorMessage = error.message;
+          console.log(errorCode);
+          if (errorCode === "auth/user-not-found") {
+            setsignInErrorText("No user with these credentials exists");
+          } else {
+            setsignInErrorText("Something went wrong with sign in");
+          }
         });
 
       console.log("Signing in, user: " + data.get("email"));
@@ -335,7 +334,7 @@ function SignIn(props) {
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="h2">
             Sign in
           </Typography>
           <Box
@@ -368,6 +367,9 @@ function SignIn(props) {
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             />
+            <Typography component="h1" variant="caption" sx={{ color: "red" }}>
+              {signInErrorText}
+            </Typography>
             <Button
               type="submit"
               fullWidth
@@ -399,6 +401,8 @@ function SignIn(props) {
 // Email Link Verification Component
 
 function EmailLink() {
+  const [emailLinkErrorText, setemailLinkErrorText] = useState("");
+
   const handleEmailLinkSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -422,22 +426,27 @@ function EmailLink() {
       // },
       // dynamicLinkDomain: "example.page.link",
     };
-
-    sendSignInLinkToEmail(auth, data.get("email"), actionCodeSettings)
-      .then(() => {
-        // The link was successfully sent. Inform the user.
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
-        window.localStorage.setItem("emailForSignIn", data.get("email"));
-        let emaillog = window.localStorage.getItem("emailForSignIn");
-        console.log("Stored email: " + emaillog);
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ...
-      });
+    if (!isEmail(data.get("email"))) {
+      setemailLinkErrorText("Not a valid email, please enter a valid email");
+    } else {
+      sendSignInLinkToEmail(auth, data.get("email"), actionCodeSettings)
+        .then(() => {
+          // The link was successfully sent. Inform the user.
+          // Save the email locally so you don't need to ask the user for it again
+          // if they open the link on the same device.
+          window.localStorage.setItem("emailForSignIn", data.get("email"));
+          let emaillog = window.localStorage.getItem("emailForSignIn");
+          console.log("Stored email: " + emaillog);
+          setemailLinkErrorText("Email Link Sent!");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          // ...
+        });
+    }
   };
 
   return (
@@ -474,6 +483,7 @@ function EmailLink() {
               autoComplete="email"
               autoFocus
             />
+
             <Button
               type="submit"
               fullWidth
@@ -482,6 +492,9 @@ function EmailLink() {
             >
               Send Email Link
             </Button>
+            <Typography component="h1" variant="caption" sx={{ color: "red" }}>
+              {emailLinkErrorText}
+            </Typography>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
@@ -502,15 +515,187 @@ function EmailLink() {
   );
 }
 
+// Forgot Password form (created by copying Sign In component)
+function ForgotPassword() {
+  const [forgotErrorText, setforgotErrorText] = useState("");
+
+  const handleForgotSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+    });
+
+    if (!isEmail(data.get("email"))) {
+      setforgotErrorText("Not a valid email, please enter a valid email");
+    } else {
+      sendPasswordResetEmail(auth, data.get("email"))
+        .then(() => {
+          // Password reset email sent!
+          console.log("Password reset email sent! to: " + data.get("email"));
+          setforgotErrorText("Password reset email sent!");
+          // ..
+        })
+        .catch((error) => {
+          console.log("no user with this email exists");
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          if (errorCode === "auth/user-not-found") {
+            setforgotErrorText("No user with this email exists");
+          } else {
+            setforgotErrorText("Something went wrong with sign in");
+          }
+          // ..
+        });
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Forgot Password
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleForgotSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Send Password Reset Email
+            </Button>
+            <Typography component="h1" variant="caption" sx={{ color: "red" }}>
+              {forgotErrorText}
+            </Typography>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+// Delete user component
+
+function DeleteUser(props) {
+  const [deleteUserErrorText, setdeleteUserErrorText] = useState("");
+
+  const handleDeleteAccountSubmit = (event) => {
+    event.preventDefault();
+    console.log(props.currentUser);
+
+    if (window.confirm("Do you really want to delete your account?")) {
+      deleteUser(props.currentUser)
+        .then(() => {
+          console.log("user deleted: ");
+          setdeleteUserErrorText("Account deleted successfully")
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          // An error ocurred
+          if (errorCode === "auth/requires-recent-login") {
+            setdeleteUserErrorText("For sensitive operations such as account deletion the user must have logged in recently. Please log out and log in and try again")
+          } else {
+            setdeleteUserErrorText("Something went wrong with Delete")
+            console.log("Something went wrong with Delete");
+          }
+          // ..
+        });
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5" align="center">
+            Delete Account
+          </Typography>
+          <Typography component="h1" variant="body1" align="center">
+            Warning! This will delete your account and data forever. Click the
+            button below if you want to proceed.
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleDeleteAccountSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Delete My Account
+            </Button>
+            <Typography component="h1" variant="caption" align="center" sx={{ color: "red" }}>
+            {deleteUserErrorText}
+          </Typography>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
 function App() {
   const [signedInUsername, setsignedInUsername] = useState("Logged Out");
   const [signInSuccessfulText, setSignInSuccessfulText] = useState("");
+
+  // the current logged in user
+  let currentUser = auth.currentUser;
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
       const uid = user.uid;
+      currentUser = user;
       console.log(
         "Auth state changed, user is: " + uid + " username: " + user.email
       );
@@ -523,8 +708,6 @@ function App() {
     }
   });
 
-
-  
   function GoogleHandleClick(event) {
     console.log("Google Sign in clicked: ");
 
@@ -607,9 +790,11 @@ function App() {
           Log Out
         </Button>
       </Box>
-      <SignUp setSignInSuccessfulText={setSignInSuccessfulText}/>
-      <SignIn setSignInSuccessfulText={setSignInSuccessfulText}/>
+      <SignUp setSignInSuccessfulText={setSignInSuccessfulText} />
+      <SignIn setSignInSuccessfulText={setSignInSuccessfulText} />
       <EmailLink />
+      <ForgotPassword />
+      <DeleteUser currentUser={currentUser}/>
       <GoogleLoginButton
         onClick={GoogleHandleClick}
         style={{ width: "30%", margin: "5px auto 0", display: "block" }}
