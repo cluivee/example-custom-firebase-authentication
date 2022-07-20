566a51aa5c59fe833c968b0596f14ecd02af39f0
Clive <cluivee@gmail.com>
2022-07-20 16:08:43 +0100

    something confusing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import './App.css'
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
@@ -29,6 +30,7 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Switch from '@mui/material/Switch';
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
@@ -42,16 +44,30 @@ import {
   FacebookLoginButton,
   GoogleLoginButton,
 } from "react-social-login-buttons";
+import { FormGroup } from "@mui/material";
+
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApO_RmF_i4oZglgdAaiZwHNO4mWDCsbO8",
-  authDomain: "auth-firebaseauthentication.firebaseapp.com",
-  projectId: "auth-firebaseauthentication",
-  storageBucket: "auth-firebaseauthentication.appspot.com",
-  messagingSenderId: "83111206827",
-  appId: "1:83111206827:web:f7bc2274def0a45bf9fcee",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+// Keeping this here because I might use this for Github pages anyway
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyApO_RmF_i4oZglgdAaiZwHNO4mWDCsbO8',
+//   authDomain: 'auth-firebaseauthentication.firebaseapp.com',
+//   projectId: 'auth-firebaseauthentication',
+//   storageBucket: 'auth-firebaseauthentication.appspot.com',
+//   messagingSenderId: '83111206827',
+//   appId: '1:83111206827:web:f7bc2274def0a45bf9fcee',
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -767,9 +783,39 @@ function DeleteUser() {
               {deleteUserErrorText}
             </Typography>
           </Box>
+          <Typography
+              component="h1"
+              variant="h5"
+              align="center"
+            >
+              Dark Theme
+            </Typography>
+            <SwitchLabels />
         </Box>
       </Container>
     </ThemeProvider>
+  );
+}
+
+function SwitchLabels() {
+  const [labelText, setlabelText] = useState('Dark Theme Off')
+
+  const onSwitchChange = () => {
+    if (labelText === 'Dark Theme Off'){
+      setlabelText('Dark Theme On')
+
+      document.documentElement.style.setProperty('--background-App', '#7F8487')
+
+    } else {
+      setlabelText('Dark Theme Off')
+      document.documentElement.style.setProperty('--background-App', 'transparent')
+    }
+  }
+
+  return (
+    <FormGroup>
+      <FormControlLabel control={<Switch onChange={onSwitchChange}/>} label={labelText} />
+    </FormGroup>
   );
 }
 
@@ -782,6 +828,8 @@ function App() {
   /* putting onAuthStateChanged in useEffect sets the onauthstate listener only once when App is first rendered, preventing
   another listener being added when App is rerendered, thereby preventing infinite loops when we change the state in onauthstatechanged.
   This solution took about 2 hours to find: https://stackoverflow.com/questions/61155701/how-to-prevent-infinite-loop-caused-by-onauthstatechanged-firebase-auth.
+  Edit: Could have used useAuthState hook from react-firebase-hooks also, which also gives you access to a 'user' variable telling you if
+  they are logged in or not. That might have been easier.
   */
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -852,9 +900,12 @@ function App() {
         <Button
           color="primary"
           onClick={() => setshowComponent("ForgotPassword")}
+          style={{marginRight: 'auto'}}
         >
           Forgot Password
         </Button>
+        <div style={{marginRight: '8px', fontWeight:'700'}}>{signedInUsername}</div>
+
         <Button
           fullWidth
           variant="contained"
@@ -862,7 +913,7 @@ function App() {
             width: "10%",
             justifyContent: "center",
             float: "right",
-            marginLeft: "auto",
+
             marginRight: "8px",
           }}
           onClick={() => {
@@ -879,7 +930,7 @@ function App() {
           Log Out
         </Button>
       </header>
-      <div>{signedInUsername}</div>
+      
       <Box textAlign="center"></Box>
 
       <Switcher />
